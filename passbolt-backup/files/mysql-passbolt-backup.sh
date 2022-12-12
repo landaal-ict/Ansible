8d53ce46c8dc3e0a22eca1a8abe8139d73736d73
landaal-ict <wesley@familielandaal.nl>
2022-12-12 11:10:44 +0100

    add cronjob and mysql backup with date

--- passbolt-backup/files/mysql-passbolt-backup.sh
+++ passbolt-backup/files/mysql-passbolt-backup.sh
@@ -3,4 +3,4 @@
 
 docker exec -i mysql-passbolt bash -c \
   'mysqldump -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}' \
-  > /pool/database_backups/passbolt_db.sql
+  > /pool/database_backups/passbolt_db--$(date +"%Y%m%d%H%M").sql
