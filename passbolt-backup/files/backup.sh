6b67f921d1fa6c3ba800fa6bc13dff6f09078fda
landaal-ict <wesley@familielandaal.nl>
2022-12-12 15:12:39 +0100

    add backup

---
+++ passbolt-backup/files/backup.sh
@@ -0,0 +1,4 @@
+docker cp passbolt:/etc/passbolt/gpg/serverkey_private.asc \
+    /pool/passbolt_backup/serverkey_private.asc #backup location
+docker cp passbolt:/etc/passbolt/gpg/serverkey.asc \
+    /pool/passbolt_backup/serverkey.asc #backup location
